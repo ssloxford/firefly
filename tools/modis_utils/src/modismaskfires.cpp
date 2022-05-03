cd5e7424a676eccb18a26571397e9822dcfe58a3
Edd Salkield <edd@salkield.uk>
2022-05-03 18:15:44 +0100

    modismaskfires: erase IR channels

--- tools/modis_utils/src/modismaskfires.cpp
+++ tools/modis_utils/src/modismaskfires.cpp
@@ -22,20 +22,14 @@ int main(int argc, char *argv[]) {
   }
 
   CCSDSPacket<giis::SecondaryHeader, giis::DataField> packet;
-  // CCSDSPacket packet;
-  // int n = 0;
   while (std::cin >> packet) {
-    //n++;
-    //std::cerr << "outputting packet" << n << "," << packet.data_len() << '\n';
-    /*
     for (int ifov=1; ifov<=5; ifov++) {
       // Mask out the infrared channels
       packet.data_field.data_word(ifov, 21) = 0;
       packet.data_field.data_word(ifov, 22) = 0;
     }
-    */
-    std::cout << packet << std::flush;
 
-    //std::cerr << "end of loop" << std::endl;
+    // TODO: when the segfaulting bug on operator >> is fixed, remove the flush
+    std::cout << packet << std::flush;
   }
 }
