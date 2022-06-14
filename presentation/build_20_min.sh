db342127473a23865b424e5c05c6063dc17f39ff
Edd Salkield <edd@salkield.uk>
2022-06-14 11:41:24 +0100

    Apply short presentation polish

--- presentation/build_20_min.sh
+++ presentation/build_20_min.sh
@@ -1,1 +1,1 @@
-find . | entr -s "pdflatex firefly_20_min.tex; bibtex firefly_20_min && pdflatex firefly_20_min.tex"
+find . | entr -s "pdflatex firefly_20_min.tex; bibtex firefly_20_min && pdflatex firefly_20_min.tex; pdflatex -jobname notes firefly_20_min.tex"
